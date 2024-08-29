1c0dd239fe795240b4be6c15a8803ba7aff5d0fc
Mammadbayli <javad@mammadbayli.com>
2024-08-29 23:56:13 +0000

    handle user_send_spacket and offline_mesage_hook hooks

--- src/mod_send_packet.erl
+++ src/mod_send_packet.erl
@@ -3,31 +3,32 @@
 -behaviour(gen_mod).
 
 -export([start/2,
-        stop/1, 
-        mod_options/1,
-        mod_opt_type/1, 
-        depends/2, 
-        mod_doc/0]).
+         stop/1, 
+         mod_options/1,
+         mod_opt_type/1, 
+         depends/2, 
+         mod_doc/0]).
 
--export([process_packet/1]).
+-export([process_packet/1,
+	 process_packet_offline/1]).
 
 -include("scram.hrl").
 -include("xmpp.hrl").
 -include("logger.hrl").
-%% Required by ?T macro
 -include("translate.hrl"). 
+-include("mod_muc_room.hrl").
 
 start(Host, _Opts) ->
     ?INFO_MSG("starting mod_send_packet", []),
-    ejabberd_hooks:add(user_send_packet, Host, ?MODULE,
-                       process_packet, 50),
+    ejabberd_hooks:add(user_send_packet, Host, ?MODULE, process_packet, 50),
+    ejabberd_hooks:add(offline_message_hook, Host, ?MODULE, process_packet_offline, 50),
     ok.
 
 stop(Host) ->
     ?INFO_MSG("stopping mod_send_packet", []),
-    ejabberd_hooks:delete(user_send_packet, Host, ?MODULE,
-                          process_packet, 50),
-    ok.
+     ejabberd_hooks:delete(user_send_packet, Host, ?MODULE, process_packet, 50),
+     ejabberd_hooks:delete(offline_message_hook, Host, ?MODULE, process_packet_offline, 50),
+     ok.
 
 depends(_Host, _Opts) ->
     [].
@@ -46,31 +47,49 @@ mod_opt_type(auth_token) ->
 mod_opt_type(remote_url) ->
   fun iolist_to_binary/1.
 
-process_packet({#message{type = T} = Packet, _C2SState} = Acc)
-  when T == chat; T == groupchat ->
-    ?INFO_MSG("Send packet", []),
-    ok = filter_message(Packet),
+process_packet({#message{type = T} = Packet, _C2SState} = Acc) when T == chat ->
+    ?INFO_MSG("Send packet hook ~p~n", [Packet]),
+    Vhost = (Packet#message.to)#jid.lserver,
+    BadgeCount = mod_offline:get_queue_length((Packet#message.to)#jid.luser, Vhost),
+    forward_message(Packet, BadgeCount),
     Acc;
 
 process_packet(Acc) ->
     Acc.
 
-filter_message(#message{from = _From, to = _To, body = Body} = Msg) ->
-    case xmpp:get_text(Body) of
-	<<"">> ->
-	    ok;
-	_ ->
-           forward_message(Msg, 1)
-    end.
+process_packet_offline({Action, Packet} = Acc) when (Packet#message.type == chat) ->
+    ?INFO_MSG("Send packet offline chat ~p~n", [Packet]),
+    Acc;
+
+process_packet_offline({Action, Pkt} = Acc) ->
+    ?INFO_MSG("Send packet offline groupchat ~p~n", [Pkt]),
+    Packet = unwrap_message(Pkt),
+    Vhost = (Packet#message.to)#jid.lserver,
+    BadgeCount = mod_offline:get_queue_length((Packet#message.to)#jid.luser, Vhost),
+    forward_message(Packet, BadgeCount),
+    Acc;
+
+process_packet_offline(Acc) ->
+    Acc.
+
+unwrap_message(#message{type = normal} = Msg) ->
+    case misc:unwrap_mucsub_message(Msg) of
+	#message{} = InnerMsg ->
+	    InnerMsg;
+	false ->
+	    Msg
+    end;
+
+unwrap_message(Stanza) ->
+    Stanza.
 
 forward_message(Packet, BadgeCount) ->
-    Host = (Packet#message.to)#jid.lserver,
-    
-    RemoteUrl = gen_mod:get_module_opt(Host, ?MODULE, remote_url),
-    Token = gen_mod:get_module_opt(Host, ?MODULE, auth_token),
+    To = Packet#message.to,
+
+    RemoteUrl = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, remote_url),
+    Token = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, auth_token),
   
-    %%Stanza = fxml:element_to_binary(xmpp:encode(Packet)),
-     Stanza = binary_to_list(fxml:element_to_binary(xmpp:encode(Packet))),
+    Stanza = binary_to_list(fxml:element_to_binary(xmpp:encode(Packet))),
      
      Data = string:join(["{",
         "\"stanza\": \"", Stanza, "\", ",
@@ -79,3 +98,4 @@ forward_message(Packet, BadgeCount) ->
 
     Request = {binary_to_list(RemoteUrl), [{"Authorization", binary_to_list(Token)}], "application/json", Data},
     httpc:request(post, Request,[],[]).
+
